67157940dc12e171ff1be1ca866f6e18e6e37eef
khuephamy <khuephumy19@gmail.com>
2023-03-19 12:46:53 -0500

    weather temperature to main.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,12 +6,42 @@ import {Header} from "../Hedaer/Header";
 import {Main} from "../Main/Main";
 import {api} from '../../utils/weatherApi';
 import { location, API_KEY } from '../../utils/constants';
-
+import { CurrentTemperatureUnitContext } from "../../context/CurrentTemperatureUnitContext";
 function App() {
+  const [weatherData, setWeatherData] = useState({});
+  const [selectedCard, setSelectedCard] = useState(null);
+  const [clothingitems, setClothingItems] = useState([]);
+  const [isImagePreviewOpen, setIsImagePreviewOpen] = useState(false);
+  const [currentTemperatureUnit, setCurrentTemperatureUnit] = useState("F");
+
+  const handleCardClick = (card) => {
+    setSelectedCard(card);
+    setIsImagePreviewOpen(true);
+  };
+
+  const handleToggleSwitchChange = () => {
+    currentTemperatureUnit === "F"
+    ? setCurrentTemperatureUnit("C")
+    : setCurrentTemperatureUnit("F");
+  }
+   
+
+
+
+
   return (
     <div className="App">
-      <Header />
-      <Main />
+       {/* <CurrentTemperatureUnitContext.Provider
+          value={{ currentTemperatureUnit, handleToggleSwitchChange }}
+        ></CurrentTemperatureUnitContext.Provider> */}
+      <Header 
+      weatherData= {weatherData}
+      />
+      <Main 
+       weatherData={weatherData}
+       cards={clothingitems}
+       onCardClick={handleCardClick}
+       />
     </div>
   );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,35 +4,37 @@ import { ItemCard } from "../ItemCard/ItemCard";
 import { CurrentTemperatureUnitContext } from "../../context/CurrentTemperatureUnitContext";
 import "./Main.css";
 
-
 export function Main({ weatherData, cards, onCardClick }) {
-
-  const { currentTemperatureUnit } = React.useContext(
+  const currentTemperatureUnit = React.useContext(
     CurrentTemperatureUnitContext
   );
 
-  // let temperature = weatherData.main?.temp;
-  // const weatherType = () => {
-  //   if (temperature >= 86) {
-  //     return "hot";
-  //   } else if (temperature >= 66 && temperature <= 85) {
-  //     return "warm";
-  //   } else if (temperature <= 65) {
-  //     return "cold";
-  //   }
-  // };
+  let temperature = weatherData.temperature;
+  const weatherType = () => {
+    if (temperature >= 86) {
+      return "hot";
+    } else if (temperature >= 66 && temperature <= 85) {
+      return "warm";
+    } else if (temperature <= 65) {
+      return "cold";
+    }
+  };
 
   return (
     <main className="main">
-      <WeatherCard />
+      <WeatherCard weatherData={weatherData} />
       <section className="main__clothes-container">
-        <div className="main__description-container">
-          <p className="main__description">
-            Today is 75F/ You may want to wear:
-          </p>
+        <div className="main__info">
+          <div className="main__description-container">
+            <p className="main__description">
+              Today is {temperature}°F and it is {weatherType}
+            </p>
+            <p className="main__descritipn_slash"> / </p>
+            <p className="main__description">You may want to wear:</p>
+          </div>
         </div>
         <ul className="main__items">
-          <ItemCard/>
+          <ItemCard />
         </ul>
       </section>
     </main>
