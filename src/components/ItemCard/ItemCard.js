95313917f976e147f257797ec4fe2e2d80faa765
khuephamy <khuephumy19@gmail.com>
2023-03-19 19:22:28 -0500

    card title

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./ItemCard.css";
-export function ItemCard({card, onCardClick}) {
+export function ItemCard({ card, onCardClick }) {
   return (
     <div className="card" onClick={() => onCardClick(card)}>
       <div className="card__content">
         <h2 className="card__title">{card.name}</h2>
-        <img className="card__image" src={card.link} alt={card.name}/>
       </div>
-     
+      <img className="card__image" src={card.link} alt={card.name} />
     </div>
-  )
+  );
 }
