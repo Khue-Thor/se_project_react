5e29ef5f4fa5c1c1a4238bef5c7ecac2c373b38e
khuephamy <khuephumy19@gmail.com>
2023-03-24 13:15:48 -0500

    delete uneeded font styles

--- src/components/ToggleSwitch/ToggleSwitch.js
+++ src/components/ToggleSwitch/ToggleSwitch.js
@@ -3,18 +3,20 @@ import "./ToggleSwitch.css";
 import { CurrentTemperatureUnitContext } from "../../context/ CurrentTemperatureUnitContext";
 
 export function ToggleSwitch() {
-  const { CurrentTemperatureUnit, handleToggleSwitchChange } = React.useContext();
+  // const { currentTemperatureUnit, handleToggleSwitchChange } = React.useContext(
+  //   CurrentTemperatureUnitContext
+  // );
 
   return (
     <div className="toggleswitch__container">
-      <label>
+      <label className="toggleswitch">
         <p className="toggleswitch__f">F</p>
         <p className="toggleswitch__c">C</p>
         <input
         className="toggleswitch__input"
         type="checkbox"
-        value={CurrentTemperatureUnit}
-        onClick={handleToggleSwitchChange}
+        // value={currentTemperatureUnit}
+        // onClick={handleToggleSwitchChange}
         />
         <span className="toggleswitch__slider"></span>
       </label>
