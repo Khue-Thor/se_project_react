5a85c371302870bf438350bb0f05761428c5a4cf
khuephamy <khuephumy19@gmail.com>
2023-03-24 18:02:43 -0500

    styles ClothesSection

--- src/components/ClothesSection/ClothesSection.js
+++ src/components/ClothesSection/ClothesSection.js
@@ -6,7 +6,7 @@ export function ClothesSection({ cards, handleAddClick, onCardClick }) {
   return (
     <section className="clothes__section">
       <div className="clothes__section-info">
-        <h2 className="clothes__section-title">Your Item</h2>
+        <h2 className="clothes__section-title">Your items</h2>
         <button className="clothes__add-button" type="button" onClick={handleAddClick}>
           + Add new
         </button>
