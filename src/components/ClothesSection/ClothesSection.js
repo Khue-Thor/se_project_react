427dab0c8bcc61733a0fb52dc4be2422a505fe97
khuephamy <khuephumy19@gmail.com>
2023-03-25 11:26:58 -0500

    styles delete modal

--- src/components/ClothesSection/ClothesSection.js
+++ src/components/ClothesSection/ClothesSection.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { ItemCard } from "../ItemCard/ItemCard";
 import "./ClothesSection.css";
-import { DeleteConfirmationModal } from "../DeleteConfirmationModal/DeleteConfirmationModal";
 
 export function ClothesSection({ cards, handleAddClick, onCardClick }) {
   return (
@@ -16,7 +15,6 @@ export function ClothesSection({ cards, handleAddClick, onCardClick }) {
         {cards.map((card) => {
           <ItemCard key={card._id} card={card} onCardClick={onCardClick} />;
         })}
-        <DeleteConfirmationModal/>
       </ul>
     </section>
   );
--- src/components/DeleteConfirmationModal/DeleteConfirmationModal.js
+++ src/components/DeleteConfirmationModal/DeleteConfirmationModal.js
@@ -1,17 +1,18 @@
 import React from "react";
 import "./DeleteConfirmationModal.css";
 
-export function DeleteConfirmationModal() {
+export function DeleteConfirmationModal({onClose, handleDelete}) {
   return (
     <div className="modal__confirm">
       <div className="modal__confirm-container">
+      <button className='modal__close' onClick={onClose} />
         <div className="modal__messages-container">
           <p className="modal__message">Are you sure you want to delete this item?</p>
           <p className="modal__message">This action is irreversible.</p>
         </div>
         <di className="modal__buttons-container">
-          <button className="modal__delete_confirm">Yes, delete item</button>
-          <button className="modal__delete_cancel">Cancel</button>
+          <button className="modal__delete_confirm" onClick={handleDelete}>Yes, delete item</button>
+          <button className="modal__delete_cancel" onClick={onClose}>Cancel</button>
         </di>
       </div>
     </div>
