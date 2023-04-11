01bc4655c35e6a32435d9e0e261d7a724f7a65cd
khuephamy <khuephumy19@gmail.com>
2023-04-11 13:33:30 -0500

    styles change profile data modal

--- src/components/EditProfileModal/EditProfileModal.js
+++ src/components/EditProfileModal/EditProfileModal.js
@@ -3,10 +3,13 @@ import { ModalWithForm } from "../ModalWithForm/ModalWithForm";
 import { useHistory } from "react-router-dom";
 
 export function EditProfileModal({ isOpen, isLoading, currentUser, onEditProfile, onCloseModal }) {
-
   const [name, setName] = useState("");
   const [avatar, setAvatar] = useState("");
   const history = useHistory();
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+  }
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -21,7 +24,32 @@ export function EditProfileModal({ isOpen, isLoading, currentUser, onEditProfile
       onSubmit={handleSubmit}
       closeModal={onCloseModal}
     >
-      <label>Name</label>
+      <fieldset className="modal__form-fieldset">
+        <label className="modal__form-label">Name*</label>
+        <input
+          id="name"
+          type="text"
+          name="name"
+          value={name}
+          placeholder="Name"
+          minLength="1"
+          maxLength="30"
+          onChange={handleNameChange}
+          className="modal__form-input"
+        />
+        <span className="modal__input-error"></span>
+        <label className="modal__form-label">Avatar</label>
+        <input
+          id="avatar-url"
+          type="url"
+          name="avatar"
+          value={avatar}
+          placeholder="Avatar url"
+          onChange={handleNameChange}
+          className="modal__form-input"
+        />
+        <span className="modal__input-error"></span>
+      </fieldset>
     </ModalWithForm>
   );
 }
