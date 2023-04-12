71a83629224ced1ff8d886715212d696a0baaa88
khuephamy <khuephumy19@gmail.com>
2023-04-12 14:58:03 -0500

    update API-TOKEN

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_KEY = "d54726166f875fbd211ff49b398e1392";
+export const API_KEY = "66d79d686e2c2216a220c2059ac437b7";
 export const location = { latitude: "44.954445", longitude: "-93.091301" };
 
 export const currentDate = new Date().toLocaleString("default", {
