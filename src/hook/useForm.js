9731ca18a9738eb50d4563aa909ec771ecdc0332
khuephamy <khuephumy19@gmail.com>
2023-04-14 08:53:12 -0500

    create useForm hook

---
+++ src/hook/useForm.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+
+export function useForm(inputValues) {
+  const [values, setValues] = useState(inputValues);
+
+  const handleChange = (e) {
+    const {value, name} = e.target;
+    setValues({ ...value, [name]: value});
+  };
+
+  return {values, handleChange, setValues};
+}
